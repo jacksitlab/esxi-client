8ba671d4d6f3e58a7320e2ec3276fd4a9c266fcb
Michael DÜrre <michael.duerre@highstreet-technologies.com>
2021-11-23 07:37:24 +0100

    add libonly file

---
+++ esxiClientLib.py
@@ -0,0 +1,107 @@
+import argparse
+import urllib3
+import getpass
+import json
+import yaml
+from lib.loginResponse import LoginResponse
+from lib.createVmInfoResponse import CreateVmInfoResponse
+from lib.getVmInfoResponse import GetVmInfoResponse
+from lib.getHostInfoResponse import GetHostInfoResponse
+from lib.summaryData import SummaryData
+from lib.esxiRequests import EsxiRequests
+
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.114 Safari/537.36"
+urllib3.disable_warnings()
+class EsxiClient:
+
+    def __init__(self, host, username, password, scheme="https", port=443):
+        self.host = host
+        self.credentials = dict(username=username, password=password)
+        self.scheme = scheme
+        self.port = port
+        self.loginData = None
+
+    def login(self):
+        response = self.requestRest('POST','/sdk/',EsxiRequests.Login(self.credentials['username'],self.credentials['password']))
+#        print(response)
+        if response["code"] == 200:
+            self.loginData = LoginResponse(response['content'],response['headers'])
+            return True
+        else:
+            return False
+
+    def createGuestInfos(self):
+        response = self.requestRest('POST', '/sdk/',EsxiRequests.CreateGuestInfos())
+        if response['code'] == 200:
+            data = CreateVmInfoResponse(response['content'])
+            return data.sessionKey
+        return None
+
+    def createHostInfos(self):
+        response = self.requestRest('POST', '/sdk/',EsxiRequests.CreateHostInfos())
+        if response['code'] == 200:
+            data = CreateVmInfoResponse(response['content'])
+            return data.sessionKey
+        return None
+
+    def getGuestInfos(self, sessionkey):
+        response = self.requestRest('POST','/sdk/',EsxiRequests.RequestGuestInfos(sessionkey))
+        if response['code']==200:
+            return GetVmInfoResponse(response['content'])
+        return None
+
+    def getHostInfos(self, sessionkey):
+        response = self.requestRest('POST','/sdk/',EsxiRequests.RequestHostInfos(sessionkey))
+        if response['code']==200:
+            return GetHostInfoResponse(response['content'])
+        return None
+
+
+    def requestRest(self, method, url, data=None, headers=dict()):
+        http = urllib3.PoolManager(cert_reqs='CERT_NONE')
+        url='{}://{}{}'.format(self.scheme, self.host, url)
+        headers["Content-Type"]="text/xml"
+        headers["Accept="]=": */*"
+        headers["Referer"]="{}://{}/ui/".format(self.scheme,self.host)
+        headers["Host"]=self.host
+        headers["User-Agent"]=USER_AGENT
+        headers["SOAPAction"]="urn:vim25/s55741"
+        headers["Connection"]="keep-alive"
+        if self.loginData is not None:
+            headers['Cookie']=self.loginData.getCookie()
+        r = None
+        if data == None:
+            r = http.request(method, url, headers=headers)
+        else:
+            encoded_data = data.encode('utf-8')
+#            print("payload="+str(encoded_data))
+            r = http.request(method, url, body=encoded_data, headers=headers)
+        return dict(code=r.status, content = r.data.decode('utf-8'), headers=r.headers)
+
+    def getGuestInfos(self):
+        self.login()
+        key = self.createGuestInfos()
+        data = self.getGuestInfos(key)
+        return data.toDict()
+
+    def getHostInfos(self):
+        self.login()
+        key = self.createHostInfos()
+        data = self.getHostInfos(key)
+        return data.toDict()
+    def getAllInfos(self):
+        self.login()
+        key = self.createGuestInfos()
+        data=SummaryData()
+        data.setGuests(self.getGuestInfos(key))
+        key = self.createHostInfos()
+        data.setHost(self.getHostInfos(key))
+        return data.toDict()
+
+    def testConnection(self):
+        success= self.login()
+        msg="Login succeeded" if success else "Login failed"
+        return dict(success=success, msg=msg)
+
+
+
